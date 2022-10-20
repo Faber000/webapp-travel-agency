5fbd5062ee988ef9c99b7812066dde639c992669
Fabio Moro <fabio.moro2000@gmail.com>
2022-10-20 14:51:42 +0200

    Homepage Api/axios

---
+++ Controllers/Api/PackageController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapp_travel_agency.Models;
+
+namespace webapp_travel_agency.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PackageController : ControllerBase
+    {
+        TravelAgency context;
+
+        public PackageController()
+        {
+            context = new TravelAgency();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IQueryable<PacchettoViaggio> packages;
+            
+            packages = context.Packages;
+
+            return Ok(packages);
+        }
+    }
+}
